2d5d2736f6991f7e3db5a5e0b04586a1c1fa6ec2
Alaa Zamel <alaa.zamel23@gmail.com>
2023-09-24 11:54:39 +0300

    initial configurations

---
+++ src/SolutionTemplate.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SolutionTemplate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read inputs
+        int n = sc.nextInt();
+
+
+        // Solve
+        //.
+        //.
+        //.
+
+        // Output results
+        System.out.println();
+
+        sc.close();
+    }
+
+}
